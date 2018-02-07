f3bfc0fbc71940739996b6aac9404b14553794c0
Unknown <szymon.gackowski@gmail.com>
2018-02-07 17:37:54 +0100

    Zapisywanie haseł w bazie

---
+++ database/migrations/2018_02_07_144913_create_passwords_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('email');
+            $table->string('password');
+
+            $table->dateTime('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passwords');
+    }
+}
