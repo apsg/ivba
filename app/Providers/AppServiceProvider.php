6209acfef309d02dbb6216e4776ae510ce8b1488
Unknown <szymon.gackowski@gmail.com>
2018-02-07 17:38:14 +0100

    Validator od sprawdzania 4 ostatnich haseł

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,24 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         \Carbon\Carbon::setLocale('pl');
+
+        Validator::extend('different_password', function($attribute, $value, $parameters, $validator){
+
+
+            $passwords = \DB::table('passwords')
+                ->select('password')
+                ->where('email', $validator->getData()['email'])
+                ->orderBy('created_at', 'desc')
+                ->take(4)
+                ->get();
+
+            foreach($passwords as $password){
+                if(\Hash::check($value, $password->password))
+                    return false;
+            }
+
+            return true;
+        });
     }
 
     /**
