d536bb4afc530fa1fed751daa674f44b8d41f177
Unknown <szymon.gackowski@gmail.com>
2018-09-22 16:58:09 +0200

    Zamiana miejscami

--- resources/views/buy_access.blade.php
+++ resources/views/buy_access.blade.php
@@ -9,31 +9,6 @@
 @section('content')
     <section class="page content">
         <div class="container">
-            @if( Auth::check() && Auth::user()->full_access_expires && Auth::user()->full_access_expires->isFuture() )
-
-                @if( !Auth::user()->canAddFullAccess() )
-                    Ważność Twojego konta jest dłuższa niż rok (ważne do {{ Auth::user()->full_access_expires }}). Nie
-                    można teraz przedłużyć bardziej.
-                @else
-                    <h1>Przedłuż pełen dostęp do strony</h1>
-                    <p>Masz już wykupiony pełen dostęp ważny do dnia {{ Auth::user()->full_access_expires }}, ale możesz
-                        go już teraz przedłużyć, jeśli chcesz</p>
-                    <a href="{{ url('/cart/add_full_access') }}" class="btn btn-primary">Przedłuż dostęp</a>
-                @endif
-            @else
-                <h1>Kup pełen dostęp do strony</h1>
-                <p>W tym miejscu możesz kupić roczny dostęp do WSZYSTKICH zasobów na iVBA.pl na okres 1 roku.</p>
-                <ul style="list-style: circle;">
-                    <li>Podana cena jest ceną brutto i zawiera 23% VAT - brak innych opłat</li>
-                    <li>Dostęp jest aktywowany na rok czasu - 365 dni</li>
-                    <li>Masz prawo w ciągu 30 dni zrezygnować</li>
-                </ul>
-                <br/>
-                <a href="{{ url('/cart/add_full_access') }}" class="btn btn-primary">Kup pełen dostęp
-                    za {{ config('ivba.full_access_price') }} zł brutto</a>
-                <hr/>
-            @endif
-
             <h2>Wykup abonament</h2>
             @if( Auth::check() && Auth::user()->full_access_expires && Auth::user()->full_access_expires->isFuture() )
                 <p>Masz aktywny pełen dostęp - nie możesz wykupić abonamentu</p>
@@ -81,18 +56,6 @@
                             </tr>
                             </tbody>
                         </table>
-                        {{--
-                        <label>
-                            <input type="checkbox" name="rules" id="rules">
-                            Zapoznałam/zapoznałem się i zgadzam z <a style="vertical-align: bottom;" href="{{ url('/regulamin') }}" target="_blank">regulaminem strony {{ config('app.name') }}</a>
-                        </label> --}}
-                        <br/>
-                        <br/>
-                        {{-- <form action="{{ url('/process_subscription') }}" method="post">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="amount" value="{{ config('ivba.subscription_price_first') }}">
-                            <button title="Aby wykupić abonament musisz potwierdzić zapoznanie się i zgodę z regulaminem" disabled="disabled" id="pay-button" class="btn btn-primary">Wykup abonament - zapłać pierwszą opłatę i zapisz kartę</button>
-                        </form> --}}
 
                         <h3>PayPal</h3>
 
@@ -115,6 +78,32 @@
                     <p>Zaloguj się, by wykupić abonament</p>
                     <a href="{{ url('/login') }}" class="btn btn-primary">Zaloguj</a>
                 @endif
+            @endif
+            <hr/>
+
+            @if( Auth::check() && Auth::user()->full_access_expires && Auth::user()->full_access_expires->isFuture() )
+
+                @if( !Auth::user()->canAddFullAccess() )
+                    Ważność Twojego konta jest dłuższa niż rok (ważne do {{ Auth::user()->full_access_expires }}). Nie
+                    można teraz przedłużyć bardziej.
+                @else
+                    <h1>Przedłuż pełen dostęp do strony</h1>
+                    <p>Masz już wykupiony pełen dostęp ważny do dnia {{ Auth::user()->full_access_expires }}, ale możesz
+                        go już teraz przedłużyć, jeśli chcesz</p>
+                    <a href="{{ url('/cart/add_full_access') }}" class="btn btn-primary">Przedłuż dostęp</a>
+                @endif
+            @else
+                <h1>Kup pełen dostęp do strony</h1>
+                <p>W tym miejscu możesz kupić roczny dostęp do WSZYSTKICH zasobów na iVBA.pl na okres 1 roku.</p>
+                <ul style="list-style: circle;">
+                    <li>Podana cena jest ceną brutto i zawiera 23% VAT - brak innych opłat</li>
+                    <li>Dostęp jest aktywowany na rok czasu - 365 dni</li>
+                    <li>Masz prawo w ciągu 30 dni zrezygnować</li>
+                </ul>
+                <br/>
+                <a href="{{ url('/cart/add_full_access') }}" class="btn btn-primary">Kup pełen dostęp
+                    za {{ config('ivba.full_access_price') }} zł brutto</a>
+                <hr/>
             @endif
             <hr/>
         </div>
