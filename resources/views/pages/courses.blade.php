e596070de8389f31641aa7cd6f0a369049a62a3a
Unknown <szymon.gackowski@gmail.com>
2019-03-26 19:42:20 +0100

    Show courses to everybody

--- resources/views/pages/courses.blade.php
+++ resources/views/pages/courses.blade.php
@@ -11,25 +11,7 @@
             </div>
 
             <courses></courses>
-
-            <div class="row">
-                <div class="col-sm-12">
-
-                    @if( Auth::check() && Auth::user()->hasFullAccess() )
-                        <div class="alert alert-info">Masz już pełen dostęp do wszystkich materiałów</div>
-                    @endif
-
-                    @if(!Auth::check())
-                        <div class="alert alert-danger">Zaloguj się, aby zobaczyć kursy, do których masz dostęp.</div>
-                    @endif
-
-                    @if(!empty($next))
-                        <div class="alert alert-info">Dostęp do następnego kursu uzyskasz za następującą liczbę
-                            dni: {{ $next }}</div>
-                    @endif
-
-                </div>
-            </div>
+            
         </div>
 
     </section>
