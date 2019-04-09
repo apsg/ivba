394a6c08d4f6d78a2c7741049e2c3121893d5e4b
Unknown <szymon.gackowski@gmail.com>
2019-04-09 12:54:12 +0200

    Fix relative image paths problem

--- resources/views/admin/quizzes/show.blade.php
+++ resources/views/admin/quizzes/show.blade.php
@@ -255,7 +255,10 @@
                 content_css: [
                     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
                     '//www.tinymce.com/css/codepen.min.css'
-                ]
+                ],
+                relative_urls: false,
+                remove_script_host: false,
+                convert_urls: true,
             });
         });
 
