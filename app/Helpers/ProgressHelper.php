9adf0224f6a27d09442897e1b7a773ba02f235fc
Unknown <szymon.gackowski@gmail.com>
2019-05-05 17:18:02 +0200

    IVBA-3A

--- app/Helpers/ProgressHelper.php
+++ app/Helpers/ProgressHelper.php
@@ -6,12 +6,12 @@ use App\User;
 
 class ProgressHelper
 {
-    public static function cacheKey(User $user = null, Course $course = null)
+    public static function cacheKey(User $user = null, Course $course = null) : string
     {
         return static::cacheKeyById($user->id ?? 0, $course->id ?? 0);
     }
 
-    public static function cacheKeyById(int $userId, int $courseId)
+    public static function cacheKeyById(int $userId = null, int $courseId = null) : string
     {
         return 'progress-' . $userId . '-' . $courseId;
     }
