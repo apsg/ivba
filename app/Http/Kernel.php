24750f345b5056b6a5462dd8bb70c45075a96796
Gacek <szymon.gackowski@callpage.io>
2019-05-27 13:59:27 +0200

    Disable password age check

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CheckForEmailClickedLinks::class,
-            \App\Http\Middleware\CheckPasswordAge::class,
+//            \App\Http\Middleware\CheckPasswordAge::class,
             HttpsMiddleware::class,
         ],
 
