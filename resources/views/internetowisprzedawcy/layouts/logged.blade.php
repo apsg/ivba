30d8c8fc55d168636ab272bb263beec99d01511f
Szymon Gackowski <szymon.gackowski@gmail.com>
2021-11-21 18:24:41 +0100

    Moved sidebar to section

--- resources/views/internetowisprzedawcy/layouts/logged.blade.php
+++ resources/views/internetowisprzedawcy/layouts/logged.blade.php
@@ -1,30 +1,33 @@
 @extends('layouts.html')
 
+@section('sidebar')
+    @include('partials.menu-logged')
+@endsection
+
 @section('body')
-<body class="logged">
-<div @if(request()->path() != 'cart' ) id="app" @endif>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sidebar px-1 position-fixed w-100 pt-5" id="sticky-sidebar">
-                @include('partials.menu-logged')
-            </div>
-            <div class="col-main pt-5 px-3 w-100" id="main">
-
-                <div class="">
-                    <div class="text-right">
-                        <a href="{{ url('/account') }}" class="account">
-                            @include('icons.account')
-                        </a>
-                    </div>
+    <body class="logged">
+    <div @if(request()->path() != 'cart' ) id="app" @endif>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sidebar px-1 position-fixed w-100 pt-5" id="sticky-sidebar">
+                    @yield('sidebar')
                 </div>
+                <div class="col-main pt-5 px-3 w-100" id="main">
 
-                @include('common.errors')
+                    <div class="">
+                        <div class="text-right">
+                            <a href="{{ url('/account') }}" class="account">
+                                @include('icons.account')
+                            </a>
+                        </div>
+                    </div>
 
-                @yield('content')
+                    @include('common.errors')
 
+                    @yield('content')
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
-
