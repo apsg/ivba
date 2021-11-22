18f0d8df12e8af725447433e0fa231cb3e329ca4
Szymon Gackowski <szymon.gackowski@gmail.com>
2021-11-22 21:02:28 +0100

    Corrected title

--- resources/views/internetowisprzedawcy/learn/partials/addons.blade.php
+++ resources/views/internetowisprzedawcy/learn/partials/addons.blade.php
@@ -1,5 +1,6 @@
 <div class="bg-white rounded-50 p-5 lesson-content">
-    <h3 class="lesson-header">Dodatki</h3>
+    <h3 class="lesson-header">Pobierz materiały - {{ $lesson->title }}</h3>
+    <hr />
     @foreach($lesson->items() as $item)
         <div class="border border-secondary rounded mb-3 p-3">
             @include('learn.partials.item' . $item->type)
