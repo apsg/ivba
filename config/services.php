ebaec874dd5f5140fe95894ae02abf88d450db1c
Szymon Gackowski <szymon.gackowski@gmail.com>
2022-05-07 15:12:46 +0200

    rm secret

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
 
     'fakturownia' => [
         'url'   => env('FAKTUROWNIA_URL', 'https://tmgfv.fakturownia.pl'),
-        'token' => env('FAKTUROWNIA_TOKEN', '4LKI41aAgCU8lzjdiVB7/tmgfv'),
+        'token' => env('FAKTUROWNIA_TOKEN'),
     ],
 
     'getresponse' => [
