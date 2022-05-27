b5b797c59ad52b41490c9778e42bb08f2c9af552
Gacek <szymon.gackowski@gmail.com>
2022-05-27 12:01:31 +0200

    2022-05-27_12-01-31

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/courses', CoursesController::class . '@index')->name('api.courses.index');
 
 Route::group([
