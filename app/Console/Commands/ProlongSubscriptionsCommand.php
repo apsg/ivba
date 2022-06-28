9a84bffc3144bc4f561b8ad5d8bf41b0e9b35895
Szymon Gackowski <szymon.gackowski@gmail.com>
2022-06-28 19:20:28 +0200

    2022-06-28_19-20-28

--- app/Console/Commands/ProlongSubscriptionsCommand.php
+++ app/Console/Commands/ProlongSubscriptionsCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Console\Commands;
 
 use App\Events\ActiveSubscriptionExpiredEvent;
@@ -9,35 +8,20 @@ use Illuminate\Console\Command;
 
 class ProlongSubscriptionsCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'subscriptions:prolong';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Prolong active subscriptions that have expired';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
     public function handle()
     {
+        \Log::info('Trying to prolong subscriptions');
+
         $subscriptions = Subscription::active()
             ->where('valid_until', '<', Carbon::now())
             ->get();
+
+        $count = $subscriptions->count();
+
+        \Log::info("Prolonging {$count} subscriptions");
 
         foreach ($subscriptions as $subscription) {
             event(new ActiveSubscriptionExpiredEvent($subscription));
