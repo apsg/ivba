a69d5a629799ae107c86122110342040623582e8
Szymon Gackowski <szymon.gackowski@gmail.com>
2022-09-25 16:39:22 +0200

    added granting full access via api

--- app/Domains/Api/Controllers/ExternalAccessController.php
+++ app/Domains/Api/Controllers/ExternalAccessController.php
@@ -4,18 +4,29 @@ namespace App\Domains\Api\Controllers;
 use App\Domains\Api\Events\CourseAccessGrantedEvent;
 use App\Domains\Api\Requests\GrantAccessRequest;
 use App\Domains\Courses\Services\CoursesService;
+use App\Events\FullAccessGrantedEvent;
 use App\Http\Controllers\Controller;
+use App\Repositories\AccessRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Response;
 
 class ExternalAccessController extends Controller
 {
-    public function grant(GrantAccessRequest $request, UserRepository $userRepository, CoursesService $coursesService)
-    {
+    public function grant(
+        GrantAccessRequest $request,
+        UserRepository $userRepository,
+        CoursesService $coursesService,
+        AccessRepository $accessRepository
+    ) {
         $user = $userRepository->findByEmailOrCreate($request->input('email'));
 
-        $coursesService->attachUserToCourse($user, $request->course());
-        event(new CourseAccessGrantedEvent($user, $request->course()));
+        if ($request->isFullAccess()) {
+            $accessRepository->grantFullAccess($user, 365);
+            event(new FullAccessGrantedEvent($user));
+        } else {
+            $coursesService->attachUserToCourse($user, $request->course());
+            event(new CourseAccessGrantedEvent($user, $request->course()));
+        }
 
         return response('ok', Response::HTTP_CREATED);
     }
--- app/Domains/Api/Requests/GrantAccessRequest.php
+++ app/Domains/Api/Requests/GrantAccessRequest.php
@@ -6,16 +6,26 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class GrantAccessRequest extends FormRequest
 {
-    public function rules() : array
+    public function rules(): array
     {
         return [
-            'email'     => 'required|email',
-            'course_id' => 'required|integer|exists:courses,id',
+            'email'          => 'required|email',
+            'course_id'      => 'required_without:is_full_access|integer|exists:courses,id',
+            'is_full_access' => 'required_without:course_id|boolean',
         ];
     }
 
-    public function course() : Course
+    public function course(): ?Course
     {
         return Course::find($this->input('course_id'));
+    }
+
+    public function isFullAccess(): bool
+    {
+        if (empty($this->input('is_full_access'))) {
+            return false;
+        }
+
+        return $this->input('is_full_access');
     }
 }
