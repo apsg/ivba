c0e055cde6dffddfee89aea043937bf8046871a0
Gacek <szymon.gackowski@gmail.com>
2022-11-17 20:12:10 +0100

    2022-11-17_20-12-10

--- config/stripe.php
+++ config/stripe.php
@@ -6,11 +6,10 @@ return [
     'subscription_product' => env('STRIPE_SUBSCRIPTION_PRODUCT_ID'),
     'automatic'            => [
         'full_access'  => [
-            'prod_MjardqCx01poGx', // Production
             'prod_MooRtPaHwZoSub', // test
         ],
         'subscription' => [
-            'prod_MnZebeQOIyJObC', // Production
+            'prod_MjardqCx01poGx', // Production
             'prod_MooSFl3WjyXRuU', // test
         ],
     ],
