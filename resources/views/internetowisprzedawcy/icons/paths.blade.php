71712d8304b522e25f085d8563f651e753005190
Gacek <szymon.gackowski@gmail.com>
2022-12-20 12:35:38 +0100

    new icon

--- resources/views/internetowisprzedawcy/icons/paths.blade.php
+++ resources/views/internetowisprzedawcy/icons/paths.blade.php
@@ -1,29 +1,38 @@
 <div class="d-block">
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 50 50">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+         viewBox="0 0 50 50">
         <defs>
             <clipPath id="clip-path">
-                <rect width="42" height="42" fill="none"/>
+                <rect id="Rectangle_831" data-name="Rectangle 831" width="42" height="42"/>
             </clipPath>
         </defs>
-        <g id="Group_133" data-name="Group 133" transform="translate(8992 4922)">
-            <g id="Component_61_23" data-name="Component 61 – 23" transform="translate(-8992 -4922)">
-                <g id="Repeat_Grid_3" data-name="Repeat Grid 3" clip-path="url(#clip-path)">
-                    <g transform="translate(-81 -375)">
-                        <g id="MDI_motion-play-outline" data-name="MDI / motion-play-outline" transform="translate(81.469 375.469)">
-                            <g id="Boundary" transform="translate(-0.469 -0.469)" fill="#929c9c" stroke="rgba(0,0,0,0)" stroke-width="1" opacity="0">
-                                <rect width="42" height="42" stroke="none"/>
-                                <rect x="0.5" y="0.5" width="41" height="41" fill="none"/>
+        <g id="Group_133" transform="translate(8992 4922)">
+            <g id="Component_61_23" transform="translate(-8992 -4922)">
+                <g id="Group_119" data-name="Group 119">
+                    <g id="Repeat_Grid_3" clip-path="url(#clip-path)">
+                        <g id="Group_118" data-name="Group 118" transform="translate(-81 -375)">
+                            <g id="MDI_motion-play-outline" transform="translate(81.469 375.469)">
+                                <g id="Boundary" transform="translate(-0.469 -0.469)" opacity="0">
+                                    <rect id="Rectangle_829" data-name="Rectangle 829" width="42" height="42"
+                                          fill="#6f7071"/>
+                                    <rect id="Rectangle_830" data-name="Rectangle 830" width="41" height="41"
+                                          transform="translate(-76.9 18.2)" fill="#6f7071"/>
+                                </g>
+                                <path id="Path_motion-play-outline"
+                                      d="M37.6,20.5a17.071,17.071,0,0,0-17-17.1h-.1a16.469,16.469,0,0,0-5.8,1l1.2,3.3a12.158,12.158,0,0,1,4.6-.9A13.7,13.7,0,1,1,6.8,20.5a12.667,12.667,0,0,1,.9-4.7L4.4,14.7a19.1,19.1,0,0,0-1,5.8,17.1,17.1,0,0,0,34.2,0M9.4,6.8a2.6,2.6,0,1,1,0,5.2A2.647,2.647,0,0,1,6.8,9.4V9.3A2.63,2.63,0,0,1,9.4,6.8Z"
+                                      fill="#6f7071"/>
                             </g>
-                            <path id="Path_motion-play-outline" data-name="Path / motion-play-outline" d="M25.953,19.109l-10.266-7.7m20.531,7.7A17.072,17.072,0,0,0,19.109,2a17.75,17.75,0,0,0-5.783.958l1.2,3.319a12.239,12.239,0,0,1,4.585-.907A13.739,13.739,0,1,1,5.371,19.109a12.524,12.524,0,0,1,.907-4.654L2.958,13.326A17.75,17.75,0,0,0,2,19.109a17.109,17.109,0,0,0,34.219,0M7.937,5.371A2.592,2.592,0,1,1,5.371,7.937,2.656,2.656,0,0,1,7.937,5.371Z" transform="translate(1.422 1.422)" fill="#929c9c"/>
                         </g>
                     </g>
                 </g>
             </g>
-            <g id="Group_122" data-name="Group 122" transform="translate(-9089.29 -5160.058)">
-                <path id="Path_452" data-name="Path 452" d="M118.29,264.811H112.3v2.813h11.975v-2.813Z" fill="#909c9c"/>
-                <path id="Path_453" data-name="Path 453" d="M118.29,250.492a5.154,5.154,0,0,0-5.154,5.153c0,2.846,5.154,9.166,5.154,9.166s5.153-6.32,5.153-9.166A5.153,5.153,0,0,0,118.29,250.492Zm0,6.528a1.71,1.71,0,1,1,1.71-1.711A1.711,1.711,0,0,1,118.29,257.02Z" fill="#909c9c"/>
+            <g id="Group_122" transform="translate(-9089.29 -5160.058)">
+                <path id="Path_452" d="M118.3,264.8h-6v2.8h12v-2.8Z" fill="#6f7071"/>
+                <path id="Path_453"
+                      d="M118.3,250.5a5.228,5.228,0,0,0-5.2,5.2c0,2.8,5.2,9.2,5.2,9.2s5.2-6.3,5.2-9.2a5.294,5.294,0,0,0-5.2-5.2Zm0,6.5a1.7,1.7,0,1,1,1.7-1.7,1.685,1.685,0,0,1-1.7,1.7Z"
+                      fill="#6f7071"/>
             </g>
         </g>
-</svg>
+    </svg>
 
 </div>
