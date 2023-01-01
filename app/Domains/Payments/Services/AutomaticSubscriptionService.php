af743df7f28ce66aef01b2a77eab11f2bf1d11bd
Gacek <szymon.gackowski@gmail.com>
2023-01-01 14:48:08 +0100

    new p30 welcome view

--- app/Domains/Payments/Services/AutomaticSubscriptionService.php
+++ app/Domains/Payments/Services/AutomaticSubscriptionService.php
@@ -1,13 +1,12 @@
 <?php
 namespace App\Domains\Payments\Services;
 
+use App\Domains\Admin\Helpers\SettingsHelper;
 use App\Domains\Payments\Dtos\Stripe\InvoiceDto;
-use App\Events\AutomaticSubscriptionStartedEvent;
+use App\Domains\Quicksales\Integrations\MailerliteService;
 use App\Repositories\SubscriptionRepository;
 use App\Repositories\UserRepository;
 use App\Subscription;
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Log;
 
 class AutomaticSubscriptionService
@@ -50,8 +49,13 @@ class AutomaticSubscriptionService
 
         $this->subscriptionRepository->activateOrProlongFromStripe($dto);
 
-        Log::info('Automatic subscription event');
-        event(new AutomaticSubscriptionStartedEvent($subscription));
+//        event(new AutomaticSubscriptionStartedEvent($subscription));
+
+        try {
+            app(MailerliteService::class)->addUserToGroup($user, setting(SettingsHelper::STRIPE_MAILERLITE));
+        } catch (\Exception $exception) {
+            Log::error('MAILERLITE EXCEPTION ' . $exception->getMessage());
+        }
 
         return $subscription;
     }
@@ -59,10 +63,5 @@ class AutomaticSubscriptionService
     protected function isAutomaticSubscriptionPayment(InvoiceDto $dto): bool
     {
         return in_array($dto->getProductId(), config('stripe.automatic.subscription'));
-    }
-
-    public function findAutomaticSubscriptions(Carbon $fromDate = null): Builder
-    {
-
     }
 }
--- resources/views/projekt30/welcome.blade.php
+++ resources/views/projekt30/welcome.blade.php
@@ -5,9 +5,32 @@
 @section('content')
     <section class="container">
         <div class="row">
+            <div class="col-12 my-5 py-5">
+                <div class="d-flex justify-content-between align-middle">
+                    <div>
+                        <img src="{{ asset('/images/internetowisprzedawcy/logo.png') }}" />
+                    </div>
+                    <div class="mb-5">
+                        <a class="btn btn-p30-red btn-lg p-3"
+                           style="border-radius: 7px; box-shadow: 0px 10px 30px #E7323B48;"
+                           href="https://internetowisprzedawcy.pl">
+                            Dowiedz się więcej
+                            <i class="fa fa-search ml-3"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="text-left">
+                    <p style="font-size: 16px;"><span style="color:#00A9A7">✓</span> Pełny dostęp do zaawansowanych <a href="https://internetowisprzedawcy.pl/courses">kursów z E-commerce</a></p>
+                    <p style="font-size: 16px;"><span style="color:#00A9A7">✓</span> Dedykowane miejsce na bieżące aktualności i porady o sprzedaży w Internecie</p>
+                    <p style="font-size: 16px;"><span style="color:#00A9A7">✓</span> Ćwiczenia, schematy i cykliczne raporty z postępów w wdrażaniu rozwiązań</p>
+                    <p style="font-size: 16px;"><span style="color:#00A9A7">✓</span> Dedykowane forum integrujące społeczność Internetowych sprzedawców</p>
+                </div>
+
+                <hr class="my-5" />
+            </div>
             <div class="col-md-6">
                 <iframe src="https://player.vimeo.com/video/311629551" width="380" height="285" class="w-100 h-100 rounded" style="border-radius: 30px !important;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-				
+
             </div>
             <div class="col-md-6 pt-5">
                 <div class="d-flex">
