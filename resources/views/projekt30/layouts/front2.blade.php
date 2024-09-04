803f2b9aa6e230ba10962cfc8df710ea75cf456b
gacek <szymon.gackowski@gmail.com>
2024-09-04 23:07:22 +0200

    fix bootstrap on projekt30

--- resources/views/projekt30/layouts/front2.blade.php
+++ resources/views/projekt30/layouts/front2.blade.php
@@ -12,6 +12,7 @@
 
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}"/>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/inauka_custom.css') }}" rel="stylesheet">
@@ -113,6 +114,8 @@
 @stack('scripts')
 
 @include('cookieConsent::index')
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
 </body>
 </html>
