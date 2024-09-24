ea2d868de741a32cafa08c9f1783bf224c0afc20
gacek <szymon.gackowski@gmail.com>
2024-09-24 11:44:41 +0200

    testimonials

---
+++ resources/views/inauka2/partials/ambassadors.blade.php
@@ -0,0 +1,141 @@
+<div class="container">
+    <p class="font-sora color-red">Nasi ambasadorzy</p>
+    <h4 class="h4-headline color-graphite">
+        <img src="/images/inauka2/tarcza.png"/>
+        Monitorowanie przez specjalistów
+    </h4>
+    <div class="d-block d-md-flex">
+
+        <div class="px-2">
+            <div class="ambassador rounded p-3 pt-5">
+                <div class="image">
+                    <img
+                            src="{{ url('/images/inauka2/ambassadors/konstancja.svg') }}"
+                            class="rounded rounded-50"
+                    />
+                    <div class="chevron">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                            <g id="Group_581" data-name="Group 581" transform="translate(877 -11073.56)">
+                                <rect id="Rectangle_373" data-name="Rectangle 373" width="25" height="25"
+                                      transform="translate(-877 11073.56)" fill="none"/>
+                                <path id="Path_4155" data-name="Path 4155"
+                                      d="M4.6,0,0,.017,4.041,10.11.085,20,4.7,19.983,8.656,10.11Z"
+                                      transform="translate(-854 11082.172) rotate(90)" fill="#fd5429"/>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                <div class="text-center mt-3">
+                    <h6 class="h6-headline color-graphite text-center">
+                        Konstancja
+                    </h6>
+                    <p class="color-red font-sora text-center" style="font-size:12px">
+                        AMBASADORKA
+                    </p>
+                    <img src="{{ url('/images/inauka2/ambassadors/excel.png') }}"/>
+                    <p class="body-2">Ms Excel</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="px-2">
+            <div class="ambassador rounded p-3 pt-5">
+                <div class="image">
+                    <img
+                            src="{{ url('/images/inauka2/ambassadors/michal.svg') }}"
+                            class="rounded rounded-50"
+                    />
+                    <div class="chevron">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                            <g id="Group_581" data-name="Group 581" transform="translate(877 -11073.56)">
+                                <rect id="Rectangle_373" data-name="Rectangle 373" width="25" height="25"
+                                      transform="translate(-877 11073.56)" fill="none"/>
+                                <path id="Path_4155" data-name="Path 4155"
+                                      d="M4.6,0,0,.017,4.041,10.11.085,20,4.7,19.983,8.656,10.11Z"
+                                      transform="translate(-854 11082.172) rotate(90)" fill="#fd5429"/>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                <div class="text-center mt-3">
+                    <h6 class="h6-headline color-graphite text-center">
+                        Michał
+                    </h6>
+                    <p class="color-red font-sora text-center" style="font-size:12px">
+                        AMBASADOR
+                    </p>
+                    <img src="{{ url('/images/inauka2/ambassadors/excel.png') }}"/>
+                    <p class="body-2">Ms Excel</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="px-2">
+            <div class="ambassador rounded p-3 pt-5">
+                <div class="image">
+                    <img
+                            src="{{ url('/images/inauka2/ambassadors/dominik.svg') }}"
+                            class="rounded rounded-50"
+                    />
+                    <div class="chevron">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                            <g id="Group_581" data-name="Group 581" transform="translate(877 -11073.56)">
+                                <rect id="Rectangle_373" data-name="Rectangle 373" width="25" height="25"
+                                      transform="translate(-877 11073.56)" fill="none"/>
+                                <path id="Path_4155" data-name="Path 4155"
+                                      d="M4.6,0,0,.017,4.041,10.11.085,20,4.7,19.983,8.656,10.11Z"
+                                      transform="translate(-854 11082.172) rotate(90)" fill="#fd5429"/>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                <div class="text-center mt-3">
+                    <h6 class="h6-headline color-graphite text-center">
+                        Dominik
+                    </h6>
+                    <p class="color-red font-sora text-center" style="font-size:12px">
+                        AMBASADOR
+                    </p>
+                    <img src="{{ url('/images/inauka2/ambassadors/excel.png') }}"/>
+                    <p class="body-2">Ms Excel</p>
+                </div>
+            </div>
+        </div>
+        <div class="px-2">
+            <div class="ambassador rounded p-3 pt-5">
+                <div class="image">
+                    <img
+                            src="{{ url('/images/inauka2/ambassadors/katarzyna.svg') }}"
+                            class="rounded rounded-50"
+                    />
+                    <div class="chevron">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                            <g id="Group_581" data-name="Group 581" transform="translate(877 -11073.56)">
+                                <rect id="Rectangle_373" data-name="Rectangle 373" width="25" height="25"
+                                      transform="translate(-877 11073.56)" fill="none"/>
+                                <path id="Path_4155" data-name="Path 4155"
+                                      d="M4.6,0,0,.017,4.041,10.11.085,20,4.7,19.983,8.656,10.11Z"
+                                      transform="translate(-854 11082.172) rotate(90)" fill="#fd5429"/>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                <div class="text-center mt-3">
+                    <h6 class="h6-headline color-graphite text-center">
+                        Katarzyna
+                    </h6>
+                    <p class="color-red font-sora text-center" style="font-size:12px">
+                        AMBASADORKA
+                    </p>
+                    <img src="{{ url('/images/inauka2/ambassadors/excel.png') }}"/>
+                    <p class="body-2">Ms Excel</p>
+                </div>
+            </div>
+        </div>
+        <div class="px-2 flex-grow-1">
+            <div class="px-3 w-100" style="background-color: #FF6841">
+                <a href="{{ url('/ambasadorzy') }}" class="color-red">Zobacz pozostałych</a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/inauka2/partials/moving_banner.blade.php
+++ resources/views/inauka2/partials/moving_banner.blade.php
@@ -1,12 +1,12 @@
-<div class="border border-3 rounded border-color-red row px-5 py-4" style="background-color: #FD54290F;">
-    <div class="col-6">
-        <h2 class="h2-headline">
+<div class="border border-3 rounded border-color-red row flex-column-reverse flex-md-row px-5 py-4" style="background-color: #FD54290F;">
+    <div class="col-12 col-md-6  text-center text-md-start">
+        <h2 class="h2-headline text-center text-md-start">
             Dołącz do
         </h2>
-        <h2 class="h2-headline color-red">
+        <h2 class="h2-headline color-red  text-center text-md-start">
             27.000 osób
         </h2>
-        <h2 class="h2-headline">
+        <h2 class="h2-headline  text-center text-md-start">
             na iNauka.pl
         </h2>
 
@@ -37,7 +37,7 @@
         </a>
 
     </div>
-    <div class="col-6">
+    <div class="col-12 col-md-6">
         <dotlottie-player
                 src="{{ url('/images/inauka2/animacja.json') }}"
                 background="transparent" speed="1" style="width: 100%" loop autoplay></dotlottie-player>
