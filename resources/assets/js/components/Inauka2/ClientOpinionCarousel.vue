47d9ecd9e285af63ffc92e33a0b0e19fafd2d0c3
PUMCZACZ <pawel.cebernik02@gmail.com>
2024-09-24 21:37:18 +0200

    add opinions to carousel

---
+++ resources/assets/js/components/Inauka2/ClientOpinionCarousel.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="container">
+    <carousel :paginationEnabled="false"
+              :perPage="3"
+              :navigate-to="page"
+              :mouseDrag="true"
+              :autoplay="true"
+              :autoplayTimeout="5000">
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-12.svg" alt="avatar logo" height="60" />
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Szkolenia iNauka pozwoliły mi na prowadzeniu szeregu usprawnień w Firmie. Pomimo ciągłej pracy na Excelu, moje analizy są teraz zdecydowanie bardziej profesjonalne.
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Łukasz Kutyłowski</span> <br>
+                  <span class="body-2">Opinia przesłana przez e-mail</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-05.svg" alt="avatar logo" height="60"/>
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Przede wszystkim dziękuję za ogromną dawkę wiedzy i pokazanie narzędzi takich jak Power Query! Już mam kilka pomysłów jak
+              mogłabym wykorzystać tą wiedzę w pracy.
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Ewelina Wysocka</span> <br>
+                  <span class="body-2">Opinia przesłana przez e-mail</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-13.svg" alt="avatar logo" height="60" />
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Polecam! Fajny sposób na przypomnienie sobie pracy w Excelu jak i możliwość nauczenia się nowych rzeczy 💪
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Katarzyna Tkacz-Duda</span> <br>
+                  <span class="body-2">Opinia przesłana komentarz na Facebooku</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-19.svg" alt="avatar logo" height="60" />
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Jestem od dawna Waszą wielką fanką. :) Robicie cudowną robotę na Mega Sobotach i maratonach.
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Agata Szulc-Maj</span> <br>
+                  <span class="body-2">Opinia przesłana przez e-mail</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-06.svg" alt="avatar logo" height="60" />
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Dziękuje za bardzo interesujące i dokładnie przeprowadzone szkolenie. Bardzo dużo nowych informacji.
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Paweł Kabat</span> <br>
+                  <span class="body-2">Opinia przesłana przez e-mail</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-17.svg" alt="avatar logo" height="60"/>
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Szkolenie świetne, jest świetnie prowadzone. Mateusz top of the top! Dzięki!
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Aleksandra Bala</span> <br>
+                  <span class="body-2">Opinia przesłana przez e-mail</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="p-4">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img src="/images/inauka2/opinions/People Avatar-02-09.svg" alt="avatar logo" height="60" />
+            </div>
+            <div class="col-12 color-graphite-light mb-5">
+              Jesteście Panowie bardzo komunikatywni i życzliwie nastawieni do uczestników, którzy są różni. Gratuluję profesjonalizmu!
+            </div>
+            <div class="col-12">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <span class="fw-bold subtitle-2">Julita Kacprowicz</span> <br>
+                  <span class="body-2">Opinia przesłana przez e-mail</span>
+                </div>
+                <div class="mt-auto">
+                  <img src="/images/inauka2/inauka-logo.svg" alt="logo" height="60" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import {Carousel, Slide} from 'vue-carousel';
+
+export default {
+  name: "ClientOpinionCarousel",
+
+  components: {Carousel, Slide},
+
+  data () {
+    return {
+      page: 0,
+    }
+  },
+
+  methods: {
+    onPageChange (page) {
+      this.page = page;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
