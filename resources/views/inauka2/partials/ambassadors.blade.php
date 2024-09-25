51acaeabdf0471d8d350f1075d1bb56b0e4da77c
gacek <szymon.gackowski@gmail.com>
2024-09-25 11:59:43 +0200

    ambassadors finished

--- resources/views/inauka2/partials/ambassadors.blade.php
+++ resources/views/inauka2/partials/ambassadors.blade.php
@@ -2,9 +2,9 @@
     <p class="font-sora color-red">Nasi ambasadorzy</p>
     <h4 class="h4-headline color-graphite">
         <img src="/images/inauka2/tarcza.png"/>
-        Monitorowanie przez specjalistów
+        Monitorowane przez specjalistów
     </h4>
-    <div class="d-block d-md-flex">
+    <div class="d-block d-md-flex" style="max-width: 100%; overflow-x: auto;">
 
         <div class="px-2">
             <div class="ambassador rounded p-3 pt-5">
@@ -133,8 +133,17 @@
             </div>
         </div>
         <div class="px-2 flex-grow-1">
-            <div class="px-3 w-100" style="background-color: #FF6841">
-                <a href="{{ url('/ambasadorzy') }}" class="color-red">Zobacz pozostałych</a>
+            <div class="px-3 w-100 rounded d-flex flex-column justify-content-center ambassadors-more">
+                <a href="{{ url('/ambasadorzy') }}" class="color-red w-100 text-center" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                        <g id="Group_582" data-name="Group 582" transform="translate(-11073.56 -852) rotate(-90)">
+                            <rect id="Rectangle_373" data-name="Rectangle 373" width="25" height="25" transform="translate(-877 11073.56)" fill="none"/>
+                            <path id="Path_4155" data-name="Path 4155" d="M4.6,0,0,.017,4.041,10.11.085,20,4.7,19.983,8.656,10.11Z" transform="translate(-854 11082.172) rotate(90)" fill="currentColor"/>
+                        </g>
+                    </svg>
+
+                    ZOBACZ POZOSTAŁYCH
+                </a>
             </div>
         </div>
     </div>
