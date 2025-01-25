d19a27994283011a528543b08edc12839692b1e7
gacek <szymon.gackowski@gmail.com>
2025-01-25 18:29:38 +0100

    inauka2

--- resources/assets/js/components/Inauka2/FaqMain.vue
+++ resources/assets/js/components/Inauka2/FaqMain.vue
@@ -64,11 +64,13 @@ export default {
       <div class="col-12 col-md-5">
         <div class="card border-0 bg-transparent my-3"
              v-for="card in cards"
-             :key="card.id">
-          <button v-on:click="setCollapsedId(card.id)" class="w-100 btn btn-link text-start color-graphite bg-color-white-box rounded"
+             :key="card.id"
+             :class="{'selected-card' : card.id === collapsedId}">
+          <button v-on:click="setCollapsedId(card.id)"
+                  class="w-100 btn btn-link text-start color-graphite bg-color-white-box rounded"
                   style="text-decoration: none !important;">
             <div class="color-graphite p-2"
-                 v-bind:class="{'selected-card' : card.id === collapsedId}">
+            >
               <span>{{ card.title }}</span>
               <div class="pt-2 text-black d-md-none" v-if="card.id === collapsedId" v-html="card.description">
               </div>
@@ -84,9 +86,24 @@ export default {
         </div>
       </div>
     </div>
+    <p class="p-3 font-sora text-uppercase">
+      Masz inne pytania?
+      <a
+        class="color-red "
+        href="mailto:hello@inauka.pl">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+          <g id="Group_582" data-name="Group 582" transform="translate(-11073.56 -852) rotate(-90)">
+            <rect id="Rectangle_373" data-name="Rectangle 373" width="25" height="25"
+                  transform="translate(-877 11073.56)" fill="none"/>
+            <path id="Path_4155" data-name="Path 4155" d="M4.6,0,0,.017,4.041,10.11.085,20,4.7,19.983,8.656,10.11Z"
+                  transform="translate(-854 11082.172) rotate(90)" fill="currentColor"/>
+          </g>
+        </svg>
+        Napisz do nas na hello@inauka.pl
+      </a>
+    </p>
   </div>
 </template>
 
 <style scoped lang="scss">
-
 </style>
